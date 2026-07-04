6953c037e5c2a1c6228b8e863eb7af92ff08aa46
Juan Contreras Merida <meridajuancontreras@gmail.com>
2026-07-03 20:37:35 -0400

    subiendo mi rama contacto juan

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import React from "react";
 import Historia from "./pages/Historia";
+import Contacto from "./pages/Contacto";
 
 function App() {
   return (
       <>
-      <Historia />
+      <Contacto />
       </>
   );
 }
